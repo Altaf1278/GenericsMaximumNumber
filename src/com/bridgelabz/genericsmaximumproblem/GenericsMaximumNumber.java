5ea1d7cb1a322304c0339e0bb276214fe1e1a52b
altaf <altaf98khan66@gmail.com>
2023-04-05 11:27:23 +0530

    Add: Find the maximum   between  3 Strings

--- src/com/bridgelabz/genericsmaximumproblem/GenericsMaximumNumber.java
+++ src/com/bridgelabz/genericsmaximumproblem/GenericsMaximumNumber.java
@@ -3,23 +3,19 @@ package com.bridgelabz.genericsmaximumproblem;
 public class GenericsMaximumNumber {
 
 	
-		 public static <A extends Comparable<A>> A maximum (A x, A y, A z){
-			 
-			 A max = x;
-			 
-			 if (y.compareTo(max) > 0) {
-				 max = y;
-				 
-			 }
-			 if (z.compareTo(max)>0) {
-				 max = z;
-			 }
-			 
-			 return max;
-			 
-		 }
-		 public static void main(String[] args) {
-			System.out.println ( "Maximum Float Value is : " +maximum(5.4, 6.9, 1.42));
-		 }
-	}
+	public static <A extends Comparable<A>> A maximum(A x, A y, A z) {
+        A max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Maximum String Value is : " + maximum("A", "K", "X"));
+    }
+}
 
