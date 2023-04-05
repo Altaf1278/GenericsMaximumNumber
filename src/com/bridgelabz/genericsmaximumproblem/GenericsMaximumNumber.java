69bd56e4c2c10c26e2ca52db8766c7b5ab7af6c3
altaf <altaf98khan66@gmail.com>
2023-04-05 23:39:45 +0530

    Add: Extend max method to take more than 3 parameters

--- src/com/bridgelabz/genericsmaximumproblem/GenericsMaximumNumber.java
+++ src/com/bridgelabz/genericsmaximumproblem/GenericsMaximumNumber.java
@@ -1,21 +1,36 @@
 package com.bridgelabz.genericsmaximumproblem;
 
+
+
 public class GenericsMaximumNumber {
 
-	
-	public static <A extends Comparable<A>> A maximum(A x, A y, A z) {
-        A max = x;
-        if (y.compareTo(max) > 0) {
-            max = y;
-        }
-        if (z.compareTo(max) > 0) {
-            max = z;
-        }
-        return max;
-    }
+	public static <A extends Comparable<A>> A maximum(A x, A y, A z, A u, A v) {
 
-    public static void main(String[] args) {
-        System.out.println("Maximum String Value is : " + maximum("A", "K", "X"));
-    }
+		A max = x;
+
+		if (y.compareTo(max) > 0) {
+			max = y;
+
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		if (u.compareTo(max) > 0) {
+			max = u;
+		}
+		if (v.compareTo(max) > 0) {
+			max = v;
+		}
+
+		
+		return max;
+
+		 
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Maximum String Value : " + maximum('A', 'K', 'S', 'T', 'G'));
+	}
+
 }
-
